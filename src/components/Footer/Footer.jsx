f372479eda0b8044d5033c5dc0fea8b97f0e4f9d
David <Rigby335@hotmail.com>
2023-10-15 12:56:29 -0700

    home/footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,15 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://linkedin.com/in/david-rigby2022">LinkedIn</a>
-      <a href="https://github.com/Rigby-David">GitHub</a>
+      <a title="LinkedIn" href="https://linkedin.com/in/david-rigby2022">
+        <img src="../../../public/linkedin-svgrepo-com.svg" />
+      </a>
+      <a title="GitHub" href="https://github.com/Rigby-David">
+        <img src="../../../public/github-142-svgrepo-com.svg" />
+      </a>
+      <a title="Resume" href="https://github.com/Rigby-David">
+        <img src="../../../public/resume-business-cv-work-job-curriculum-2-svgrepo-com.svg" />
+      </a>
     </footer>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,30 +15,35 @@ export default function Home() {
   textTimer();
 
   return (
-    <div className="home">
-      <div className="home-h1">
-        <h1>David Rigby</h1>
+    <>
+      <div className="home-and-img">
+        <div className="home">
+          <div className="home-h1">
+            <h1>David Rigby</h1>
+          </div>
+          <h3>Software Engineer</h3>
+          <section className="home-greeting-text">
+            {time === 0 && (
+              <span>
+                <em>
+                  I&apos;m a results-oriented problem-solver, collaborator, and
+                  creator.
+                </em>
+              </span>
+            )}
+            {time === 1 && (
+              <span>
+                <em>I&apos;m a React Developer.</em>
+              </span>
+            )}
+            <span>
+              <strong>Hey, I&apos;m David. </strong>
+              I&apos;m a software engineer living in Estacada, OR.
+            </span>
+          </section>
+        </div>
+        <img src="../../../public/head-shot.jpg" />
       </div>
-      <h3>Software Engineer</h3>
-      <section className="home-greeting-text">
-        {time === 0 && (
-          <span>
-            <em>
-              I&apos;m a results-oriented problem-solver, collaborator, and
-              creator.
-            </em>
-          </span>
-        )}
-        {time === 1 && (
-          <span>
-            <em>I&apos;m a React Developer.</em>
-          </span>
-        )}
-        <span>
-          <strong>Hey, I&apos;m David. </strong>
-          I&apos;m a software engineer living in Estacada, OR.
-        </span>
-      </section>
-    </div>
+    </>
   );
 }
