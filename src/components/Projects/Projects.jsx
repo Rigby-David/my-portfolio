74a5c6757d85d95685350c7509bdea19049d123e
David <Rigby335@hotmail.com>
2023-10-16 14:30:44 -0700

    re-styling projects. mobile view first

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,38 +2,13 @@ import "./Projects.css";
 
 export default function Projects() {
   return (
-    <>
-      <div className="projects-container">
-        <section className="carrot-container">
-          <img
-            className="carrot"
-            src="https://www.svgrepo.com/show/510041/left-arrow.svg"
-          />
-        </section>
-        <section className="project-image-container">
-          <img
-            className="project-image"
-            src="/Screenshot 2023-10-15 161950.png"
-          />
-        </section>
-        <section className="carrot-container">
-          <img
-            className="carrot"
-            src="https://www.svgrepo.com/show/510165/right-arrow.svg"
-          />
-        </section>
+    <div className="projects-page">
+      <div className="project-header">CinemaScout</div>
+      <img className="project-image" src="/Screenshot 2023-10-15 161950.png" />
+      <div className="links">
+        <a href="">Code</a>
+        <a href="">Live Link</a>
       </div>
-      <div className="description-container">
-        <section className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </section>
-      </div>
-    </>
+    </div>
   );
 }
