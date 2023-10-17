03e9a01e55f7499646c763f207a93aa6419c9340
David <Rigby335@hotmail.com>
2023-10-16 17:24:31 -0700

    resized footer and added third project basics

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import "./Projects.css";
 export default function Projects() {
   const [cinemaScout, setCinemaScout] = useState(false);
   const [allegori, setAllegori] = useState(false);
+  const [prints, setPrints] = useState(false);
 
   return (
     <div className="projects-page">
@@ -51,6 +52,32 @@ export default function Projects() {
               onClick={() => setAllegori(true)}
               className="project-image"
               src="/allegori.png"
+            />
+          )}
+        </div>
+        <div className="links">
+          <a href="">Code</a>
+          <a href="">Live Link</a>
+        </div>
+      </div>
+      <div className="cinema-container">
+        <div className="project-header">Printstagram</div>
+        <div className="content-container">
+          {prints ? (
+            <div className="desc" onClick={() => setPrints(false)}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          ) : (
+            <img
+              onClick={() => setPrints(true)}
+              className="project-image"
+              src="/prints.png"
             />
           )}
         </div>
