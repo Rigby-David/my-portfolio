dcfec11b94c662743612b9f100389f1488faac44
David <Rigby335@hotmail.com>
2023-10-20 08:57:58 -0700

    about content and styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,10 +4,25 @@ export default function About() {
   return (
     <div className="about">
       <div className="about-info">
-        <div className="about-text">
-          <p>I&apos;m David and here&apos;s some information about me!</p>
+        <div className="about-bio">
+          <span>I&apos;m a Full-Stack software engineer.</span>
+          <span>I love turning an idea into a reality through </span>
+          <span>collaboration and code. With over 6 years </span>
+          <span>experience as a lead installer in the </span>
+          <span>specialty concrete resurfacing industry, </span>
+          <span>and small businesses, I&apos;m excited to apply </span>
+          <span>my leadership and technical skills </span>
+          <span>to tech where my contributions will </span>
+          <span>transform the world around me into </span>
+          <span>a dynamic and accessible place for all.</span>
         </div>
-        <div className="about-tech"></div>
+        <div className="about-personal">
+          <span>When I&apos;m not coding, you can find me </span>
+          <span>at my local disc golf course, reading a </span>
+          <span>fantasy novel, or working in my yard. </span>
+          <span>Now, please enjoy this photo of my 9 month old </span>
+          <span>lab mix, Petunia, doing a big blep.</span>
+        </div>
       </div>
       <div className="about-image">
         <img src="/DavidToot.jpg" className="profile-pic" />
