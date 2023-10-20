819c8a776077c5e5c1674c2f0010f6d74fd52926
David <Rigby335@hotmail.com>
2023-10-20 09:29:27 -0700

    styled mobile view of about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,23 +5,17 @@ export default function About() {
     <div className="about">
       <div className="about-info">
         <div className="about-bio">
-          <span>I&apos;m a Full-Stack software engineer.</span>
-          <span>I love turning an idea into a reality through </span>
-          <span>collaboration and code. With over 6 years </span>
-          <span>experience as a lead installer in the </span>
-          <span>specialty concrete resurfacing industry, </span>
-          <span>and small businesses, I&apos;m excited to apply </span>
-          <span>my leadership and technical skills </span>
-          <span>to tech where my contributions will </span>
-          <span>transform the world around me into </span>
-          <span>a dynamic and accessible place for all.</span>
+          I love turning an idea into a reality through collaboration and code.
+          With over 6 years experience as a lead installer in the specialty
+          concrete resurfacing industry, and small businesses, I&apos;m excited
+          to apply my leadership and technical skills to tech where my
+          contributions will transform the world around me into a dynamic and
+          accessible place for all.
         </div>
         <div className="about-personal">
-          <span>When I&apos;m not coding, you can find me </span>
-          <span>at my local disc golf course, reading a </span>
-          <span>fantasy novel, or working in my yard. </span>
-          <span>Now, please enjoy this photo of my 9 month old </span>
-          <span>lab mix, Petunia, doing a big blep.</span>
+          When I&apos;m not coding, you can find me at my local disc golf
+          course, reading a fantasy novel, or working in my yard. Now, please
+          enjoy this photo of my 9 month old lab mix, Petunia, doing a big blep.
         </div>
       </div>
       <div className="about-image">
