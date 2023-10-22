fc224f55d7a1f4c6a35a97210f759bd04b5fd5c1
David <Rigby335@hotmail.com>
2023-10-22 12:03:57 -0700

    setup contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,30 @@
-import './Contact.css'
+import "./Contact.css";
 
 export default function Contact() {
-  return <div className="contact">
-    <div className="contact-info">
-      <h3>Thanks for checking out my profile!</h3>
-      <p>I&apos;d love to conntect with you. Feel free to reach out about anything! I&apos;m always happy to discuss my projects, tech, disc golf, yard work, and books. </p>
+  return (
+    <div className="contact">
+      <div className="contact-info">
+        <h1>Thanks for checking out my profile</h1>
+        <p>
+          Please reach out and connect with me. I&apos;m currently looking for
+          opportunities and am #OPENTOWORK. Let&apos;s chat about how I be an
+          asset to your team.
+        </p>
+      </div>
+      <div className="contact-links">
+        <p>
+          Contact me any time through <a href="">Email</a>
+        </p>
+        <p>
+          Check out my <a href="">Resume</a>
+        </p>
+        <p>
+          See the work I&apos;ve done on <a href="">GitHub</a>
+        </p>
+        <p>
+          Let&apos;s connect on <a href="">Linkedin</a>
+        </p>
+      </div>
     </div>
-    <div className="contact-links">
-      <a href="">Email</a>
-      <a href="">Resume</a>
-      <a href="">GitHub</a>
-      <a href="">Linkedin</a>
-    </div>
-  </div>;
+  );
 }
