9afcf6e70dc4b0a3f61c3a6aa741f5471621ed2e
David Rigby <rigby335@hotmail.com>
2023-10-22 10:43:41 -0700

    Contact setup and imports

--- src/components/Contact.jsx
+++
@@ -1,3 +0,0 @@
-export default function Contact() {
-  return <div>LinkedIn, Email, Phone</div>;
-}
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import './Contact.css'
+
+export default function Contact() {
+  return <div className="contact">
+    <div className="contact-info">
+      <h3>Thanks for checking out my profile!</h3>
+      <p>I&apos;d love to conntect with you. Feel free to reach out about anything! I&apos;m always happy to discuss my projects, tech, disc golf, yard work, and books. </p>
+    </div>
+    <div className="contact-links">
+      <a href="">Email</a>
+      <a href="">Resume</a>
+      <a href="">GitHub</a>
+      <a href="">Linkedin</a>
+    </div>
+  </div>;
+}
