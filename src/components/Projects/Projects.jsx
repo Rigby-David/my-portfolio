5bab991d31aec9506eafe4e1cff73694a2fbb169
David <Rigby335@hotmail.com>
2023-10-22 09:29:24 -0700

    commit for merge, small changes in projects.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,85 +7,93 @@ export default function Projects() {
   const [prints, setPrints] = useState(false);
 
   return (
-    <div className="projects-page">
-      <div className="project-container">
-        <div className="project-header">CinemaScout</div>
-        <div className="content-container">
-          {cinemaScout ? (
-            <div className="desc" onClick={() => setCinemaScout(false)}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <>
+      <div className="projects-info">
+        <div>Hi here;s some info about me</div>
+        <div className="projects-page">
+          <div className="project-container">
+            <div className="project-header">CinemaScout</div>
+            <div className="content-container">
+              {cinemaScout ? (
+                <div className="desc" onClick={() => setCinemaScout(false)}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              ) : (
+                <img
+                  onClick={() => setCinemaScout(true)}
+                  className="project-image"
+                  src="/Screenshot 2023-10-15 161950.png"
+                />
+              )}
             </div>
-          ) : (
-            <img
-              onClick={() => setCinemaScout(true)}
-              className="project-image"
-              src="/Screenshot 2023-10-15 161950.png"
-            />
-          )}
-        </div>
-        <div className="links">
-          <a href="">Code</a>
-          <a href="">Live Link</a>
+            <div className="links">
+              <a href="">Code</a>
+              <a href="">Live Link</a>
+            </div>
+          </div>
+          <div className="project-container">
+            <div className="project-header">Allegori</div>
+            <div className="content-container">
+              {allegori ? (
+                <div className="desc" onClick={() => setAllegori(false)}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              ) : (
+                <img
+                  onClick={() => setAllegori(true)}
+                  className="project-image"
+                  src="/allegori.png"
+                />
+              )}
+            </div>
+            <div className="links">
+              <a href="">Code</a>
+              <a href="">Live Link</a>
+            </div>
+          </div>
+          <div className="project-container">
+            <div className="project-header">Printstagram</div>
+            <div className="content-container">
+              {prints ? (
+                <div className="desc" onClick={() => setPrints(false)}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              ) : (
+                <img
+                  onClick={() => setPrints(true)}
+                  className="project-image"
+                  src="/prints.png"
+                />
+              )}
+            </div>
+            <div className="links">
+              <a href="">Code</a>
+              <a href="">Live Link</a>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="project-container">
-        <div className="project-header">Allegori</div>
-        <div className="content-container">
-          {allegori ? (
-            <div className="desc" onClick={() => setAllegori(false)}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-          ) : (
-            <img
-              onClick={() => setAllegori(true)}
-              className="project-image"
-              src="/allegori.png"
-            />
-          )}
-        </div>
-        <div className="links">
-          <a href="">Code</a>
-          <a href="">Live Link</a>
-        </div>
-      </div>
-      <div className="project-container">
-        <div className="project-header">Printstagram</div>
-        <div className="content-container">
-          {prints ? (
-            <div className="desc" onClick={() => setPrints(false)}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-          ) : (
-            <img
-              onClick={() => setPrints(true)}
-              className="project-image"
-              src="/prints.png"
-            />
-          )}
-        </div>
-        <div className="links">
-          <a href="">Code</a>
-          <a href="">Live Link</a>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
